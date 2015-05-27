57d057371cb5655127bae99b2e0a5092ee0b6455
redaphid <iam@aaronherres.com>
2015-05-27 14:27:23 -0700

    don't rely on android-mock in actual code

--- arduino-extern.hpp
+++ arduino-extern.hpp
@@ -1,20 +1,28 @@
-#ifndef arduino_extern_h
-#define arduino_extern_h
+#ifndef ARDUINO_H
+#define ARDUINO_H
 
-// #ifndef INPUT
-// #define INPUT 1
-// #endif
-//
-// #include <stdint.h>
-//
-// typedef uint8_t byte;
-//
-// extern void pinMode(uint8_t pin, uint8_t mode);
-// extern void digitalWrite(uint8_t pin, uint8_t mode);
-// extern void analogWrite(uint8_t pin, uint8_t mode);
-// extern int digitalRead(uint8_t pin);
-// extern int  analogRead(uint8_t pin);
+#ifndef INPUT
+#define INPUT 1
+#endif
 
-#include "arduino-mock/Arduino.h"
+#include <stdint.h>
+
+typedef uint8_t byte;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void pinMode(uint8_t pin, uint8_t mode);
+extern void digitalWrite(uint8_t pin, uint8_t mode);
+extern void analogWrite(uint8_t pin, uint8_t mode);
+extern int digitalRead(uint8_t pin);
+extern int  analogRead(uint8_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+// #include "arduino-mock/Arduino.h"
 
 #endif
